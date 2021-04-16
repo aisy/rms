fa8a6e14596a9727323db4a42fb39fb0b7322494
aisy <aisymuhammad@gmail.com>
2021-04-16 12:02:23 +0700

    add new page in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,14 +8,20 @@ import {
 import LoginPage from '../pages/Login';
 import HomePage from '../pages/Home';
 
+// not found pages if path or component not found
+import NotFound from '../pages/NotFound';
+
 const RouterApp = () => {
   return (
     <Switch>
-      <Route path={"/"} component={HomePage} />
+      <Route path={"/"} exact
+        component={HomePage} />
       <Route path={"/home"} component={HomePage} />
 
       {/* user */}
       <Route path={"/user/login"} exact component={LoginPage} />
+
+      <Route component={NotFound} />
     </Switch>
   );
 }
